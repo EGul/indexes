564f76318697dca57106350607690d6d929bf924
sarah <evangulyas@outlook.com>
2015-01-11 00:33:44 -0500

    added indexPathOf

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
-(function () {
+function indexPathOf(arr, item) {
 
-  'use strict';
+  var results = [];
 
-})();
+  for (var i = 0; i < arr.length; i++) {
+
+    if (!Array.isArray(arr[i])) {
+      if (arr[i] == item) {
+        results.push(i);
+        return results;
+       }
+    }
+    else {
+      var result = indexPathOf(arr[i], item);
+      if (result.length > 0) {
+        results.push(i);
+        result.forEach(function (e) {
+          results.push(e);
+        });
+      }
+    }
+
+  }
+
+  return results;
+}
+
+module.exports.indexPathOf = indexPathOf;
