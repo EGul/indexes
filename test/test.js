7a76189adb262b6019e8fada361a9117ff51b9ff
sarah <evangulyas@outlook.com>
2015-01-12 12:54:23 -0500

    added indexPathsOf test

--- test/test.js
+++ test/test.js
@@ -35,6 +35,37 @@ describe('indexes', function () {
 
   });
 
+  describe('indexPathsOf', function () {
+
+    it('should return -1', function () {
+
+      var tempArr = [1, 2, 3, [1, 2, [1, 2, 3]], 4];
+      var should = -1;
+
+      assert.deepEqual(should, indexes.indexPathsOf(tempArr, 10));
+
+    });
+
+    it('should return [[0], [2], [4], [5]]', function () {
+
+      var tempArr = [1, 2, 1, 3, 1, 1, 4, 5];
+      var shouldArr = [[0], [2], [4], [5]];
+
+      assert.deepEqual(shouldArr, indexes.indexPathsOf(tempArr, 1));
+
+    });
+
+    it('should return [[3], [4, 1], [4, 2, 2], [4, 2, 3, 1], [5]]', function () {
+
+      var tempArr = [1, 2, 3, 5, [1, 5, [1, 2, 5, [1, 5]]], 5];
+      var shouldArr = [[3], [4, 1], [4, 2, 2], [4, 2, 3, 1], [5]];
+
+      assert.deepEqual(shouldArr, indexes.indexPathsOf(tempArr, 5));
+
+    });
+
+  });
+
   describe('indexesOf', function () {
 
     it('should return -1', function () {
